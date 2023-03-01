730d0801e9035de37f9a433d6f23d12082a72d3b
Marshall Williams <marshall@mwthink.net>
2023-03-01 13:49:05 -0600

    sending HTML content on root page

--- src/router.js
+++ src/router.js
@@ -3,7 +3,17 @@ const Express = require('express');
 const router = Express.Router();
 
 router.get('/', (req, res) => {
-  return res.send('Hello world!');
+  return res.send(`
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Demo App</title>
+</head>
+<body>
+  <h1>Welcome to the Demo!</h1>
+</body>
+</html>
+`.trim());
 })
 
 exports.default = router;
