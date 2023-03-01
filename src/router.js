a4b1433679d4d8be726987ab372025fb7a6fa2dd
Marshall Williams <marshall@mwthink.net>
2023-03-01 14:09:38 -0600

    added counter functionality

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ const Express = require('express');
 
 const router = Express.Router();
 
+let counter = 0;
+
 router.get('/', (req, res) => {
   return res.send(`
 <!DOCTYPE html>
@@ -11,9 +13,21 @@ router.get('/', (req, res) => {
 </head>
 <body>
   <h1>Welcome to the Demo!</h1>
+  <hr/>
+  <p>The counter value is: <strong>${counter}</strong></p>
+  <br/>
+  <a href="/counter/increase"><button>Increase Counter</button></a>
 </body>
 </html>
 `.trim());
+})
+
+router.get('/counter/increase', (req, res) => {
+  // Increase the counter value
+  counter++;
+
+  // Redirect request to front-page
+  return res.redirect('/');
 })
 
 exports.default = router;
