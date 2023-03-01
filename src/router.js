5bee410031da87a696af01827a258962a3733fa4
Marshall Williams <marshall@mwthink.net>
2023-03-01 14:36:49 -0600

    added prometheus metrics

--- src/router.js
+++ src/router.js
@@ -1,11 +1,16 @@
 const Express = require('express');
+const Prometheus = require('prom-client');
 
 const router = Express.Router();
 
+const counterMetric = new Prometheus.Counter({
+  name: 'demo_counter_total',
+  help: 'The current value of the counter'
+})
 let counter = 0;
 
 router.get('/', (req, res) => {
-  return res.send(`
+  return res.contentType('html').send(`
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,14 +22,21 @@ router.get('/', (req, res) => {
   <p>The counter value is: <strong>${counter}</strong></p>
   <br/>
   <a href="/counter/increase"><button>Increase Counter</button></a>
+  <br/>
+  <a href="/metrics">Metrics</a>
 </body>
 </html>
 `.trim());
 })
 
+router.get('/metrics', async (req, res) => {
+  return res.contentType('text/plain').send(await Prometheus.register.metrics())
+})
+
 router.get('/counter/increase', (req, res) => {
   // Increase the counter value
   counter++;
+  counterMetric.inc();
 
   // Redirect request to front-page
   return res.redirect('/');
